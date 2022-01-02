be2c951b612077642d6c0ca13a768a089b048798
Anthony Mapes <anthonymapes3@gmail.com>
2022-01-01 23:15:16 -0500

    Added BoggleDice

---
+++ BoggleDice.cs
@@ -0,0 +1,21 @@
+// source: https://codegolf.stackexchange.com/questions/5654/best-scoring-boggle-board
+public static class BoggleDice {
+    public static readonly char[][] dice = {
+        new char[] {'a', 'a', 'e', 'e', 'g', 'n'},
+        new char[] {'e', 'l', 'r', 't', 't', 'y'},
+        new char[] {'a', 'o', 'o', 't', 't', 'w'},
+        new char[] {'a', 'b', 'b', 'j', 'o', 'o'},
+        new char[] {'e', 'h', 'r', 't', 'v', 'w'},
+        new char[] {'c', 'i', 'm', 'o', 't', 'u'},
+        new char[] {'d', 'i', 's', 't', 't', 'y'},
+        new char[] {'e', 'i', 'o', 's', 's', 't'},
+        new char[] {'d', 'e', 'l', 'r', 'v', 'y'},
+        new char[] {'a', 'c', 'h', 'o', 'p', 's'},
+        new char[] {'h', 'i', 'm', 'n', 'q', 'u'},
+        new char[] {'e', 'e', 'i', 'n', 's', 'u'},
+        new char[] {'e', 'e', 'g', 'h', 'n', 'w'},
+        new char[] {'a', 'f', 'f', 'k', 'p', 's'},
+        new char[] {'h', 'l', 'n', 'n', 'r', 'z'},
+        new char[] {'d', 'e', 'i', 'l', 'r', 'x'}
+    };
+}
