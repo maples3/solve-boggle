bd24e258ce0979ecdbf734281e71ddef34a331b6
Anthony Mapes <anthonymapes3@gmail.com>
2022-01-01 23:41:22 -0500

    perf test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿
-var board = BoardGenerator.generateRandomBoard();
-Console.WriteLine(board.getPrintable());
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var board = BoardGenerator.generateRandomBoard();
+        var sw = new Stopwatch();
+        sw.Start();
+
+        int iterations = 100000;
+        for(int i = 0; i < iterations; i++) {
+            board = BoardGenerator.generateRandomBoard();
+        }
+        sw.Stop();
+        Console.WriteLine($"Total time: {sw.ElapsedMilliseconds}ms");
+        Console.WriteLine($"Average time per board: {sw.ElapsedMilliseconds / (iterations * 1.0)}ms");
+    }
+}
+
